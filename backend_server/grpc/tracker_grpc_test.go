ce2e6c464bf5284759e1f56c38e3fa2f7961f3a4
Sin Ta Poon <sinta@Sins-MacBook-Pro.local>
2025-04-14 16:40:26 +0800

    test structure fix

--- backend_server/grpc/tracker_grpc_test.go
+++ backend_server/grpc/tracker_grpc_test.go
@@ -1,20 +1,8 @@
-package main
+package grpc
 
 import (
 	"testing"
-
-	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 )
-
-type StockInfo struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	Ticker      string `protobuf:"bytes,1,opt,name=ticker,proto3" json:"ticker,omitempty"`
-	Company     string `protobuf:"bytes,2,opt,name=company,proto3" json:"company,omitempty"`
-	Description string `protobuf:"bytes,3,opt,name=description,proto3" json:"description,omitempty"`
-}
 
 func TestGetTicker(t *testing.T) {
 	stock := StockInfo{
